109118e1fb740c5095de9ceee88d03b12462e6bc
Nurlan Emilbek uulu <nurlan.emilbekuulu96@gmail.com>
2023-04-03 16:18:39 +0600

    create Button page and Button ui components

---
+++ src/components/ui/Button.vue
@@ -0,0 +1,12 @@
+<script setup>
+const props = defineProps({
+  label: {
+    type: String,
+    default: 'Button'
+  }
+})
+</script>
+
+<template>
+  <button>{{ label }}</button>
+</template>
--- src/views/Button.vue
+++ src/views/Button.vue
@@ -1,3 +1,8 @@
+<script setup>
+import UiButton from '@/components/ui/Button.vue'
+</script>
+
 <template>
   <h1>Button</h1>
+  <ui-button label="Primary" />
 </template>
